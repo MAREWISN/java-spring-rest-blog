8ccb367ebfbd6f7001a4eef4cc5084a6e1200466
Marek Wiśniowski <mwisniowski@o2.pl>
2020-08-25 14:40:57 +0200

    Completed module

--- src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
+++ src/main/java/com/pluralsight/blog/data/DatabaseLoader.java
@@ -32,10 +32,10 @@ public class DatabaseLoader implements ApplicationRunner {
         IntStream.range(0,40).forEach(i->{
             String template = templates[i % templates.length];
             String gadget = gadgets[i % gadgets.length];
-            postRepository.saveAll(randomPosts);
             String title = String.format(template, gadget);
             Post post = new Post(title, "Lorem ipsum dolor sit amet, consectetur adipiscing elit… ");
             randomPosts.add(post);
+            postRepository.saveAll(randomPosts);
         });
     }
 }
